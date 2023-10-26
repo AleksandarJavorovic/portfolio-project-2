1355f4029832f9e6114b306708755139162c6e2f
AleksandarJavorovic <>
2023-10-26 13:28:59 +0000

    add function to get name and go to quiz section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 // function to hide quiz and score area at first load
 function firstLoad() {
-    
+
     document.getElementById('quiz-area').style.display = 'none';
     document.getElementById('score-area').style.display = 'none';
 };
 
 firstLoad();
+
+// function to get the name and go to Quiz section;
+function quizStart(event) {
+
+    event.preventDefault();
+
+    let playerName = document.getElementById('player-name');
+
+    document.getElementById('player-name-status').textContent = playerName.value;
+
+    document.getElementById('main-area').style.display = 'none';
+    document.getElementById('quiz-area').style.display = '';
+};
+
+let regForm = document.getElementById('reg-form');
+regForm.addEventListener('submit', quizStart);
