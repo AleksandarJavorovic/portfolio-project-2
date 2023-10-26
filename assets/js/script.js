7bbc09a28fe389562ba9b0b3d8d7f24d080923fd
AleksandarJavorovic <>
2023-10-26 12:10:52 +0000

    add function to hide quiz/score sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
+// function to hide quiz and score area at first load
+function firstLoad() {
+    
+    document.getElementById('quiz-area').style.display = 'none';
+    document.getElementById('score-area').style.display = 'none';
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+firstLoad();
